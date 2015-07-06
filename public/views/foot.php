113c39b0e5ded11b4202a5d9ce40537a035f810c
Sam <sjjyen@gmail.com>
2015-07-06 01:25:58 -0700

    split index.php into views/head.php and views/foot.php

---
+++ public/views/foot.php
@@ -0,0 +1,8 @@
+		<script src="assets/js/jquery.js"></script>
+		<script src="assets/js/jquery.cookie.js"></script>
+		<script src="assets/js/fastclick.js"></script>
+		<script src="assets/js/modernizr.js"></script>
+		<script src="assets/js/foundation.min.js"></script>
+		<script src="assets/js/samuelyen.ca.js"></script>
+	</body>
+</html>
---
+++ public/views/head.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>Resume</title>
+		<meta http-equiv="X-UA-Compatible" content="IE-edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+		<link href="assets/css/normalize.css" rel="stylesheet">
+		<link href="assets/css/foundation.min.css" rel="stylesheet">
+		<link href="assets/css/samuelyen.ca.css" rel="stylesheet">
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+	<body>
